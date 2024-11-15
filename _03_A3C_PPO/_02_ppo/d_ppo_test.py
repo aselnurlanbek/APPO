f9a1a25853bc8efc108a9c25f9e29a9a5aee26fd
aselnurlanbek <aselnurlanbekova@gmail.com>
2024-11-15 20:27:20 +0900

    Added config in test

--- _03_A3C_PPO/_02_ppo/d_ppo_test.py
+++ _03_A3C_PPO/_02_ppo/d_ppo_test.py
@@ -3,7 +3,13 @@ import os
 
 import gymnasium as gym
 import torch
-from b_actor_and_critic import MODEL_DIR, Actor
+from b_actor_and_critic import Actor
+from e_get_configs import get_environment_config
+
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+MODEL_DIR = os.path.join(CURRENT_PATH, "models")
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
 
 
 def test(env: gym.Env, actor: Actor, num_episodes: int) -> None:
@@ -30,11 +36,11 @@ def test(env: gym.Env, actor: Actor, num_episodes: int) -> None:
         print("[EPISODE: {0}] EPISODE_STEPS: {1:3d}, EPISODE REWARD: {2:4.1f}".format(i, episode_steps, episode_reward))
 
 
-def main_play(num_episodes: int, env_name: gym.Env) -> None:
-    env = gym.make(env_name, render_mode="human")
+def main_play(num_episodes: int, config) -> None:
+    env = gym.make(config["env_name"], render_mode="human")
 
-    actor = Actor(n_features=24, n_actions=4)
-    model_params = torch.load(os.path.join(MODEL_DIR, "appo_{0}_latest.pth".format(env_name)), weights_only=True)
+    actor = Actor(n_features=config["n_features"], n_actions=config["n_actions"])
+    model_params = torch.load(os.path.join(MODEL_DIR, "appo_{0}_latest.pth".format(config["env_name"])), weights_only=True)
     actor.load_state_dict(model_params)
     actor.eval()
 
@@ -45,6 +51,9 @@ def main_play(num_episodes: int, env_name: gym.Env) -> None:
 
 if __name__ == "__main__":
     NUM_EPISODES = 3
-    ENV_NAME = "BipedalWalker-v3"
+    # ENV_NAME = "BipedalWalker-v3"
 
-    main_play(num_episodes=NUM_EPISODES, env_name=ENV_NAME)
+    env_name = "lunar_lander"
+    config = get_environment_config(env_name)
+
+    main_play(num_episodes=NUM_EPISODES, config=config)
